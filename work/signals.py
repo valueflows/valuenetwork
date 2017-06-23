225e84fec51f38f22d27e4e67f9c78fdf018b92f
XaviP <pezbaloo@riseup.net>
2017-06-23 17:35:01 +0200

    Coorect links in join request comments, depending on domain.

--- work/signals.py
+++ work/signals.py
@@ -45,7 +45,6 @@ def comment_notification(sender, comment, **kwargs):
                     users.append(manager.user().user)
 
             if users:
-                #import pdb; pdb.set_trace()
                 site_name = Site.objects.get_current().name
                 domain = Site.objects.get_current().domain
                 try:
@@ -55,7 +54,6 @@ def comment_notification(sender, comment, **kwargs):
                         for pro in obj:
                             if pro == slug:
                                 site_name = comment.content_object.project.agent.name
-                                domain = kwargs['request'].get_host()
                 except:
                     pass
 
@@ -70,7 +68,7 @@ def comment_notification(sender, comment, **kwargs):
                     "site_name": site_name,
                     "joinrequest_url": joinrequest_url,
                     "jn_req": comment.content_object,
-                    "current_site": domain,
+                    "current_site": kwargs['request'].get_host(),
                     }
                 )
 

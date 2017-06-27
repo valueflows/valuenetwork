b9b03c1f736b38d122baafdd57bbd96657de17af
Bob Haugen <bob.haugen@gmail.com>
2017-06-27 11:12:37 -0500

    Add a comment about the source of the hack

--- valuenetwork/api/types/apps.py
+++ valuenetwork/api/types/apps.py
@@ -6,7 +6,16 @@ class ApiTypesAppConfig(AppConfig):
     verbose_name = "ApiTypes"
 
     def ready(self):
-        #import pdb; pdb.set_trace()
+        """ Source of this hack:
+        https://stackoverflow.com/questions/37862725/django-1-9-how-to-import-in-init-py
+        'Adding from .models import CommentMixin imports CommentMixin so that you can use it
+        inside the ready() method. It does not magically add it to the comment module so that
+        you can access it as comments.CommentMixin
+        
+        You could assign it to the comments module in the ready() method.'
+        from .models import CommentMixin
+        comments.CommentMixin = CommentsMixin
+        """
         
         from valuenetwork.api.types.EconomicResource import EconomicResource, EconomicResourceCategory
         types.EconomicResource = EconomicResource

07c4bc8638fff40c673fb3d6a317426f151e8f40
XaviP <pezbaloo@riseup.net>
2017-07-16 14:01:15 +0200

    faircoin app models in admin

--- faircoin/admin.py
+++ faircoin/admin.py
@@ -3,4 +3,20 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from faircoin.models import FaircoinAddress, FaircoinTransaction
+
+class FaircoinAddressAdmin(admin.ModelAdmin):
+    readonly_fields = ('address', 'resource',)
+    fields = ('address', 'resource',)
+    list_display = ('resource', 'address',)
+    search_fields = ['address']
+
+admin.site.register(FaircoinAddress, FaircoinAddressAdmin)
+
+class FaircoinTransactionAdmin(admin.ModelAdmin):
+    readonly_fields = ('tx_hash', 'to_address', 'event',)
+    fields = ('tx_hash', 'tx_state', 'to_address', 'event',)
+    list_display = ('event', 'tx_state', 'to_address',)
+    list_filter = ['tx_state',]
+
+admin.site.register(FaircoinTransaction, FaircoinTransactionAdmin)

6e6adcea189baff3402b86b366c615739fad2b56
Lynn Foster <foster.j.lynn@gmail.com>
2017-07-21 14:20:57 -0500

    Query for resource taxonomy items (resource types) by action

--- valuenetwork/api/schemas/ResourceTaxonomyItem.py
+++ valuenetwork/api/schemas/ResourceTaxonomyItem.py
@@ -5,7 +5,9 @@
 import graphene
 
 from valuenetwork.valueaccounting.models import EconomicResourceType
-from valuenetwork.api.types.EconomicResource import ResourceTaxonomyItem
+from valuenetwork.api.types.EconomicResource import ResourceTaxonomyItem, EconomicResourceProcessCategory
+from valuenetwork.api.types.EconomicEvent import Action
+from django.db.models import Q
 
 
 class Query(graphene.AbstractType):
@@ -18,7 +20,10 @@ class Query(graphene.AbstractType):
     all_resource_taxonomy_items = graphene.List(ResourceTaxonomyItem)
 
     resource_taxonomy_items_by_process_category = graphene.List(ResourceTaxonomyItem,
-                                                               category=graphene.String())
+                                                               category=EconomicResourceProcessCategory())
+
+    resource_taxonomy_items_by_action = graphene.List(ResourceTaxonomyItem,
+                                                      action=Action())
 
     # load single item
 
@@ -38,3 +43,19 @@ class Query(graphene.AbstractType):
     def resolve_resource_taxonomy_items_by_process_category(self, args, context, info):
         cat = args.get('category')
         return EconomicResourceType.objects.filter(behavior=cat)
+
+    def resolve_resource_taxonomy_items_by_action(self, args, context, info):
+        action = args.get('action')
+        if action == Action.WORK:
+            return EconomicResourceType.objects.filter(behavior="work")
+        if action == Action.USE:
+            return EconomicResourceType.objects.filter(behavior="used")
+        if action == Action.CONSUME:
+            return EconomicResourceType.objects.filter(behavior="consumed")
+        if action == Action.CITE:
+            return EconomicResourceType.objects.filter(behavior="cited")
+        if action == Action.PRODUCE:
+            return EconomicResourceType.objects.filter(Q(behavior="produced")|Q(behavior="used")|Q(behavior="cited")|Q(behavior="consumed"))
+        if action == Action.CHANGE:
+            return EconomicResourceType.objects.filter(Q(behavior="produced")|Q(behavior="used")|Q(behavior="cited")|Q(behavior="consumed"))
+        return None

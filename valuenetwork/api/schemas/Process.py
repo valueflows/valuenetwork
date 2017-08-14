3067f377d6a2d685607e0b3e5bb8bfb601e04c11
pospi <pospi@spadgos.com>
2017-08-14 21:18:01 +0700

    Revert "Just putting this back to a working state so I can build up what's been done and see when it breaks..."
    This reverts commit e7b29aa8ad4ed958bdcbda8ac5ac35b34e91870b.

--- valuenetwork/api/schemas/Process.py
+++ valuenetwork/api/schemas/Process.py
@@ -5,8 +5,6 @@
 # @since:   2017-06-22
 #
 
-#from django.core.exceptions import PermissionDenied
-
 from six import with_metaclass
 import graphene
 import datetime
@@ -77,8 +75,8 @@ class CreateProcess(AuthedMutation):
         return CreateProcess(process=process)
 
 
-class UpdateProcess(graphene.Mutation):
-    class Input:
+class UpdateProcess(AuthedMutation):
+    class Input(with_metaclass(AuthedInputMeta)):
         id = graphene.Int(required=True)
         name = graphene.String(required=False)
         planned_start = graphene.String(required=False)
@@ -86,7 +84,6 @@ class UpdateProcess(graphene.Mutation):
         scope_id = graphene.Int(required=False)
         note = graphene.String(required=False)
         is_finished = graphene.Boolean(required=False)
-        changed_by_id = graphene.Int(required=True)
 
     process = graphene.Field(lambda: Process)
 
@@ -99,7 +96,6 @@ class UpdateProcess(graphene.Mutation):
         note = args.get('note')
         scope_id = args.get('scope_id')
         is_finished = args.get('is_finished')
-        changed_by_id = args.get('changed_by_id')
 
         process = ProcessProxy.objects.get(pk=id)
         if process:
@@ -118,15 +114,14 @@ class UpdateProcess(graphene.Mutation):
                 process.context_agent=scope
             if is_finished:
                 process.finished=is_finished
-            changed_by = User.objects.get(pk=changed_by_id)
-            process.changed_by=changed_by
+            process.changed_by=context.user
             process.save()
 
         return UpdateProcess(process=process)
 
 
-class DeleteProcess(graphene.Mutation):
-    class Input:
+class DeleteProcess(AuthedMutation):
+    class Input(with_metaclass(AuthedInputMeta)):
         id = graphene.Int(required=True)
 
     process = graphene.Field(lambda: Process)
